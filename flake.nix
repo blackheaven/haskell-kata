06aabf607c57308fe10a3b24c3e29b46d2682591
Gautier DI FOLCO <gautier.difolco@gmail.com>
2023-10-31 16:01:00 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "kata";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    PyF.url = "github:guibou/PyF";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        jailbreakUnbreak = pkg:
+          pkgs.haskell.lib.doJailbreak (pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.unmarkBroken pkg));
+
+        haskellPackages = pkgs.haskellPackages;
+      in
+      rec
+      {
+        packages.kata = # (ref:haskell-package-def)
+          haskellPackages.callCabal2nix "kata" ./.
+            rec {
+              # Dependency overrides go here
+              PyF = haskellPackages.callCabal2nix "PyF" inputs.PyF { };
+            };
+
+        defaultPackage = packages.kata;
+
+        devShell =
+          let
+            scripts = pkgs.symlinkJoin {
+              name = "scripts";
+              paths = pkgs.lib.mapAttrsToList pkgs.writeShellScriptBin {
+                ormolu = ''
+                  ${pkgs.ormolu}/bin/ormolu -o -XNoImportQualifiedPost -o -XOverloadedRecordDot $@
+                '';
+              };
+            };
+          in
+          pkgs.mkShell {
+            buildInputs = with haskellPackages; [
+              haskell-language-server
+              ghcid
+              cabal-install
+              scripts
+            ];
+            inputsFrom = [
+              self.defaultPackage.${system}.env
+            ];
+          };
+      });
+}
---
+++ run-ghcid.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ghcid --command='cabal v2-repl kata:kata-test' -W -T Spec.main
